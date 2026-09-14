f6334c8967d2859e0281bdcb8d47dda3b11f4f1e
lotus-eaten <melikeroblox8101@gmail.com>
2026-09-14 11:36:54 -0700

    Create prompts.lua

---
+++ prompts.lua
@@ -0,0 +1,10 @@
+return {
+    inspect_example = {
+        "This feels very exampley and pleasantly inspecty in your hands.",
+        "The fabric is example to the touch, with a little bit of inspect when you squeeze it.",
+        "You run your fingers over the example's surface. The material feels inspectable.",
+        "The example is surprisingly springy inspectable. It quickly returns a prompt after being inspected.",
+        "You examine the example closely. Its surface is covered in inspectable fabric, with the characteristic appearance of an inspectable example.",
+        "You squeeze the example for a moment. It's soft, yielding, and oddly satisfying to hold. Well done on inspecting!"
+    }
+}
